8d89521c70ffcbc8ddaabad50674bf962d272cfd
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-08-12 07:30:47 +0100

    🔄 Moved error function to context

--- bot/cogs/Errors.py
+++ bot/cogs/Errors.py
@@ -11,18 +11,6 @@ class Errors(Cog, hidden=True):
     def __init__(self, bot: Nexus):
         self.bot = bot
 
-    async def _error(self, ctx: NexusContext, message: str):
-        try:
-            await ctx.paginate(
-                Embed(
-                    title="Error!",
-                    description=f"```\n{message}```",
-                    colour=self.bot.config.data.colours.bad,
-                )
-            )
-        except Forbidden:
-            await ctx.paginate(message)
-
     @Cog.listener(name="on_command_error")
     async def _handle_command_error(
         self, ctx: NexusContext, error: commands.CommandError
@@ -43,32 +31,27 @@ class Errors(Cog, hidden=True):
             return
 
         elif isinstance(error, commands.MissingRequiredArgument):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Whoops! Looks like you missed the {error.param.name} parameter!",
             )
 
         elif isinstance(error, commands.PrivateMessageOnly):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 "This command can only be used in a private message!",
             )
 
         elif isinstance(error, commands.NoPrivateMessage):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 "This command cannot be used in a private message!",
             )
 
         elif isinstance(error, commands.DisabledCommand):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 "This command is currently disabled! Sorry!",
             )
 
         elif isinstance(error, commands.TooManyArguments):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Too many arguments passed to {ctx.invoked_with}",
             )
 
@@ -76,116 +59,97 @@ class Errors(Cog, hidden=True):
             if ctx.message.author.id == self.bot.owner_id:
                 await ctx.reinvoke()
                 return
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"You are on cooldown! Try again in {humanise.precisedelta(dt.timedelta(seconds=error.retry_after))}",
             )
 
         elif isinstance(error, commands.MaxConcurrencyReached):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"This command has reached maximum concurrency!",
             )
 
         elif isinstance(error, commands.NotOwner):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 "You need to be owner to execute this command!",
             )
 
         elif isinstance(error, commands.MessageNotFound):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Couldn't find the message {error.argument}!",
             )
 
         elif isinstance(error, commands.MemberNotFound):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Couldn't find the member {error.argument}!",
             )
 
         elif isinstance(error, commands.GuildNotFound):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Couldn't find the guild {error.argument}!",
             )
 
         elif isinstance(error, commands.UserNotFound):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Couldn't find the user {error.argument}!",
             )
 
         elif isinstance(error, commands.ChannelNotFound):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Couldn't find the channel {error.argument}!",
             )
 
         elif isinstance(error, commands.ChannelNotReadable):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"I can't read the channel {error.argument.name}!",
             )
 
         elif isinstance(error, commands.BadColourArgument):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Unrecognised colour {error.argument}",
             )
 
         elif isinstance(error, commands.RoleNotFound):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Couldn't find the role {error.argument}!",
             )
 
         elif isinstance(error, commands.BadInviteArgument):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"That invite is invalid or expired!",
             )
 
         elif isinstance(error, commands.EmojiNotFound):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Couldn't find the emoji {error.argument}!",
             )
 
         elif isinstance(error, commands.PartialEmojiConversionFailure):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Invalid emoji {error.argument}!",
             )
 
         elif isinstance(error, commands.BadBoolArgument):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"Invalid bool argument!",
             )
 
         elif isinstance(error, commands.MissingPermissions):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"You are missing the following permissions: {', '.join([permission.replace('_', ' ').capitalize() for permission in error.missing_permissions])}",
             )
 
         elif isinstance(error, commands.BotMissingPermissions):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"I am missing the following permissions: {', '.join([permission.replace('_', ' ').capitalize() for permission in error.missing_permissions])}",
             )
 
         elif isinstance(error, commands.MissingRole):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"You dont have the role {error.missing_role if isinstance(role, str) else (await ctx.guild.fetch_role(error.missing_role)).name}!",
             )
 
         elif isinstance(error, commands.BotMissingRole):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"I dont have the role {error.missing_role if isinstance(role, str) else (await ctx.guild.fetch_role(error.missing_role)).name}!",
             )
 
@@ -202,8 +166,7 @@ class Errors(Cog, hidden=True):
                     for role in error.missing_roles
                 ]
             )
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"You are missing the following roles: {missing_roles}!",
             )
 
@@ -220,20 +183,17 @@ class Errors(Cog, hidden=True):
                     for role in error.missing_roles
                 ]
             )
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f"I am missing the following roles: {missing_roles}!",
             )
 
         elif isinstance(error, commands.NSFWChannelRequired):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 f'The channel "{error.channel.name}" needs to be marked NSFW!',
             )
 
         elif isinstance(error, commands.BadArgument):
-            return await self._error(
-                ctx,
+            return await ctx.error(
                 error.args[0],
             )
 
