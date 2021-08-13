9b6f784d695649c26cd62503c891190a4113d8c2
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-08-13 19:44:12 +0100

    ➕ Added custom Group

--- bot/utils/subclasses/command.py
+++ bot/utils/subclasses/command.py
@@ -1,4 +1,9 @@
-from discord.ext.commands import Command as DiscordCommand
+from discord.ext.commands import (
+    Command as DiscordCommand,
+    Group as DiscordGroup,
+    command,
+    group,
+)
 
 
 class Command(DiscordCommand):
@@ -27,3 +32,49 @@ class Command(DiscordCommand):
         )
 
         self.examples = tuple(examples)
+
+
+class Group(DiscordGroup):
+    def __init__(
+        self,
+        callback,
+        name,
+        *,
+        aliases=(),
+        brief=None,
+        description="",
+        usage=None,
+        hidden=False,
+        examples=(),
+        **kwargs
+    ):
+        super().__init__(
+            callback,
+            name=name,
+            aliases=aliases,
+            brief=brief,
+            description=description,
+            usage=usage,
+            hidden=hidden,
+            **kwargs
+        )
+
+        self.examples = tuple(examples)
+
+    def command(self, *args, **kwargs):
+        def wrapper(func):
+            kwargs.setdefault("parent", self)
+            result = command(cls=Command, *args, **kwargs)(func)
+            self.add_command(result)
+            return result
+
+        return wrapper
+
+    def group(self, *args, **kwargs):
+        def wrapper(func):
+            kwargs.setdefault("parent", self)
+            result = group(cls=Group, *args, **kwargs)(func)
+            self.add_command(result)
+            return result
+
+        return wrapper
