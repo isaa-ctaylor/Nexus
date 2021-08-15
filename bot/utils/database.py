ea9ae06bfcf90012c207f2585fdc6c90926b0dcf
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-08-15 09:39:25 +0100

    🔧 Changed database host

--- bot/utils/database.py
+++ bot/utils/database.py
@@ -16,11 +16,11 @@ class Database:
                 user=kwargs.get("user", "Nexus"),
                 password=kwargs.get("password", "nexus"),
                 database=kwargs.get("database", "Nexus"),
-                host=kwargs.get("host", "localhost"),
+                host=kwargs.get("host", "127.0.0.1"),
             )
         except asyncpg.exceptions.InvalidCatalogNameError:
             con: Connection = await asyncpg.connect(
-                database="template1", user="postgres", password="postgres", host="localhost"
+                database="template1", user="postgres", password="postgres", host="127.0.0.1"
             )
 
             await con.execute("CREATE DATABASE Nexus OWNER postgres")
