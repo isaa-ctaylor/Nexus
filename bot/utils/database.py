69798f450ca34e79c1ac9069252d3bd68ba967b5
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-08-15 09:52:34 +0100

    🤔 Even more debugging

--- bot/utils/database.py
+++ bot/utils/database.py
@@ -11,20 +11,9 @@ class Database:
         )
 
     async def new_connection(self, **kwargs):
-        try:
-            return await asyncpg.create_pool(
-                user=kwargs.get("user", "Nexus"),
-                password=kwargs.get("password", "Nexus"),
-                database=kwargs.get("database", "Nexus"),
-                host=kwargs.get("host", "127.0.0.1"),
-            )
-        except asyncpg.exceptions.InvalidCatalogNameError:
-            con: Connection = await asyncpg.connect(
-                database="template1", user="postgres", password="postgres", host="127.0.0.1"
-            )
-
-            await con.execute("CREATE DATABASE Nexus OWNER postgres")
-
-            await con.close()
-
-            return self.new_connection(**kwargs)
+        return await asyncpg.create_pool(
+            user=kwargs.get("user", "postgres"),
+            password=kwargs.get("password", "postgres"),
+            database=kwargs.get("database", "Nexus"),
+            host=kwargs.get("host", "localhost"),
+        )
