809162c4c4565770e6b4eb88e104bc883e2c0ce6
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-08-24 09:54:28 +0100

    🔧 Switch to sync approach

--- bot/cogs/Utility.py
+++ bot/cogs/Utility.py
@@ -1,10 +1,11 @@
+from utils import execute
 from io import BytesIO
 from discord.file import File
 from utils.subclasses.context import NexusContext
 from discord.ext.commands.core import is_owner
 from utils.subclasses.cog import Cog
 from utils.subclasses.bot import Nexus
-from playwright.async_api import async_playwright
+from playwright.sync_api import sync_playwright
 from utils.subclasses.command import command, Command
 from playwright._impl._api_types import TimeoutError
 from contextlib import suppress
@@ -14,6 +15,27 @@ class Utility(Cog):
     def __init__(self, bot: Nexus):
         self.bot = Nexus
 
+    @execute()
+    def _do_screenshot(self, url):
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=True)
+
+        page = browser.new_page(
+            geolocation={"latitude": 51.509865, "longitude": -0.118092},
+                viewport={"width": 1920, "height": 1080},
+                locale="en-GB",
+                permissions=["geolocation"],
+                accept_downloads=False,
+        )
+        
+        page.goto(url)
+
+        with suppress(TimeoutError):
+            page.click("text=I agree")
+
+        return BytesIO(page.screenshot())
+
+
     @is_owner()
     @command(name="screenshot", cls=Command, aliases=["ss"])
     async def _screenshot(self, ctx: NexusContext, url: str):
@@ -21,28 +43,7 @@ class Utility(Cog):
         Screenshot a website
         """
         try:
-            async with async_playwright() as playwright:
-                await ctx.send("Creating browser")
-                browser = await playwright.chromium.launch(headless=True)
-                await ctx.send("Browser created")
-                context = await browser.new_context(
-                    geolocation={"latitude": 51.509865, "longitude": -0.118092},
-                    viewport={"width": 1920, "height": 1080},
-                    locale="en-GB",
-                    permissions=["geolocation"],
-                    accept_downloads=False,
-                )
-
-            page = await context.new_page()
-            await ctx.send("Page created")
-
-            await page.goto(url)
-
-            with suppress(TimeoutError):
-                await page.click("text=I agree")
-
-            buf = BytesIO(await page.screenshot())
-
+            
             await ctx.send(file=File(buf, "screenshot.png"))
 
             await page.close()
--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,5 +1,17 @@
+from functools import partial
+from typing import Callable
 from .config import *
 from .helpers import *
 
+
 def codeblocksafe(string: Union[str, Any]):
     return str(string).replace("`", "\u200b`\u200b")
+
+
+def execute(func: Callable):
+    def inner(*args, **kwargs):
+        return asyncio.get_event_loop().run_in_executor(
+            None, partial(func, *args, **kwargs)
+        )
+
+    return inner
