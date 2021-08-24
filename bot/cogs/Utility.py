9558f019c757c19fa0301952688750685b396fb1
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-08-24 08:01:26 +0100

    🔧 Fixed screenshot command

--- bot/cogs/Utility.py
+++ bot/cogs/Utility.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from discord.file import File
 from utils.subclasses.context import NexusContext
 from discord.ext.commands.core import is_owner
@@ -12,35 +13,40 @@ from contextlib import suppress
 class Utility(Cog):
     def __init__(self, bot: Nexus):
         self.bot = Nexus
-        
+
         bot.loop.create_task(self.__ainit__())
-        
+
     async def __ainit__(self):
         async with async_playwright() as playwright:
-            self.browser = await playwright.chromium.launch()
-    
+            self.browser = await playwright.chromium.launch(headless=True)
+
     @is_owner()
     @command(name="screenshot", cls=Command, aliases=["ss"])
     async def _screenshot(self, ctx: NexusContext, url: str):
         """
         Screenshot a website
         """
-        with ctx.typing():
-            try:
-                page = await self.browser.new_page()
+        try:
+            page = await self.browser.new_page(
+                geolocation={"latitude": 51.509865, "longitude": -0.118092},
+                viewport={"width": 1920, "height": 1080},
+                locale="en-GB",
+                permissions=["geolocation"],
+                accept_downloads=False,
+            )
 
-                await page.goto(url)
-                
-                with suppress(TimeoutError):
-                    await page.click("text=I agree")
+            await page.goto(url)
 
-                buf = await page.screenshot()
-                
-                await ctx.send(file=File(buf, "screenshot.png"))
+            with suppress(TimeoutError):
+                await page.click("text=I agree")
 
-                await page.close()
-            except Exception as e:
-                await ctx.send(e)
+            buf = BytesIO(await page.screenshot())
+
+            await ctx.send(file=File(buf, "screenshot.png"))
+
+            await page.close()
+        except Exception as e:
+            await ctx.send(e)
 
 
 def setup(bot: Nexus):
