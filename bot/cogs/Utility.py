643b93e5c24cfe0455c34515b5ab3bac5a209d01
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-08-25 15:04:16 +0100

    ➕ Added redirect check command

--- bot/cogs/Utility.py
+++ bot/cogs/Utility.py
@@ -1,11 +1,34 @@
+from discord.embeds import Embed
 from utils.subclasses.cog import Cog
 from utils.subclasses.bot import Nexus
+from utils.subclasses.context import NexusContext
+from utils.subclasses.command import command, Command
+from async_timeout import timeout
+from aiohttp import InvalidURL
 
 
 class Utility(Cog):
     def __init__(self, bot: Nexus):
         self.bot = Nexus
 
+    @command(name="redirectcheck", cls=Command, aliases=["redirects", "linkcheck"])
+    async def _redirectcheck(self, ctx: NexusContext, url: str):
+        async with ctx.typing():
+            try:
+                async with timeout(30):
+                    async with self.bot.session.get(url) as resp:
+                        urls = "\n".join(str(url) for url in resp.history)
+            
+            except TimeoutError:
+                return await ctx.error("The request timed out!")
+            
+            except InvalidURL:
+                return await ctx.error("Invalid url!")
+
+            if urls:
+                await ctx.paginate(Embed(description=f"```\n{urls}```", colour=self.bot.config.data.colours.neutral))
+            else:
+                await ctx.error(f"{url} does not redirect!")
 
 
 def setup(bot: Nexus):
