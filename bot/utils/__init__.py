33e30fca854ae2191a6471808f93de8e73e49db6
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-09-14 18:13:28 +0100

    ➕ Added Timer

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,8 +1,26 @@
 from functools import wraps, partial
-from typing import Callable
+from typing import Union
 from .config import *
 from .helpers import *
+from time import sleep, time
 
 
 def codeblocksafe(string: Union[str, Any]):
     return str(string).replace("`", "\u200b`\u200b")
+
+
+class Timer:
+    _start = None
+    _end = None
+    elapsed = None
+
+    def __enter__(self):
+        self._start = time()
+        return self
+
+    def __exit__(self, *_):
+        self.end()
+
+    def end(self):
+        self._end = time()
+        self.elapsed = self._end - self._start
