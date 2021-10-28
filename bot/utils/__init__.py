df56fbb783b4b6082b30e4e9ca0bedfc3a84aeba
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-10-28 10:21:07 +0100

    ➕ Added executor

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -4,6 +4,8 @@ from .helpers import *
 from time import time
 import textwrap
 from .subclasses.command import Group, Command
+import functools
+import asyncio
 
 
 def codeblocksafe(string: Union[str, Any]):
@@ -34,3 +36,16 @@ def naturallist(iterable: Iterable, delimiter: str = ", "):
     iterable = [str(i) for i in iterable]
 
     return delimiter.join(iterable[:-1]) + f" and {iterable[-1]}"
+
+def executor(sync_function):
+    @functools.wraps(sync_function)
+    async def sync_wrapper(*args, **kwargs):
+        """
+        Asynchronous function that wraps a sync function with an executor.
+        """
+
+        loop = asyncio.get_event_loop()
+        internal_function = functools.partial(sync_function, *args, **kwargs)
+        return await loop.run_in_executor(None, internal_function)
+
+    return sync_wrapper
