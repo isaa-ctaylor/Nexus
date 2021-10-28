820d7febbf6ef50e1a0cb2fde0a0d94d4fafc1de
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-10-28 10:53:02 +0100

    🔧 .

--- bot/cogs/Minecraft.py
+++ bot/cogs/Minecraft.py
@@ -56,15 +56,19 @@ class Minecraft(Cog):
         if "error" in player:
             return await ctx.error(player["error"])
 
-        embed = Embed(title="Minecraft player info", colour=self.bot.config.colours.neutral)
-        
-        embed.set_thumbnail(url=f"https://crafatar.com/renders/body/{player['id']}?overlay")
-        
+        embed = Embed(
+            title="Minecraft player info", colour=self.bot.config.colours.neutral
+        )
+
+        embed.set_thumbnail(
+            url=f"https://crafatar.com/renders/body/{player['id']}?overlay"
+        )
+
         embed.add_field(name="Name", value=f'```\n{player["name"]}```')
-        embed.add_field(name="UUID", value=f'```\n{player["id"]}')
-        
+        embed.add_field(name="UUID", value=f'```\n{player["id"]}```')
+
         await ctx.paginate(embed)
-        
+
     @_minecraft.command(name="skin")
     async def _minecraft_skin(self, ctx: NexusContext, player: Player):
         """
@@ -72,9 +76,11 @@ class Minecraft(Cog):
         """
         if "error" in player:
             return await ctx.error(player["error"])
-        
-        embed = Embed(title=f"{player['name']}'s skin", colour=self.bot.config.colours.neutral)
-        
+
+        embed = Embed(
+            title=f"{player['name']}'s skin", colour=self.bot.config.colours.neutral
+        )
+
         embed.set_image(url=f"https://crafatar.com/renders/body/{player['id']}?overlay")
 
         await ctx.paginate(embed)
