0ce76ce0cc719b08c4ac0fa7c33d1ae620a7629d
isaa_ctaylor <isaac@isaactaylor.xyz>
2021-10-28 18:43:23 +0100

    Delete bot/utils/google directory

--- bot/utils/google/search.py
+++
@@ -1,4 +0,0 @@
-class Search:
-    def __init__(self, base_url: str = "https://www.google.com/", safe: bool = True):
-        self.url = base_url
-        self.safe = "images" if not safe else "enabled"
