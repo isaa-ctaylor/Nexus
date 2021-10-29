207fb5065721e2320ade3a738b70377ef9f33618
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-10-29 15:55:40 +0100

    🔧 Music

--- bot/cogs/Music.py
+++ bot/cogs/Music.py
@@ -1,9 +1,17 @@
+from os import getenv
+from typing import Optional
+
+from discord.channel import VoiceChannel
+from discord.ext.commands.core import guild_only
+from dotenv import load_dotenv
+from utils.subclasses.bot import Nexus
 from utils.subclasses.cog import Cog
 from utils.subclasses.command import Command
-from utils.subclasses.bot import Nexus
-import discord
-import wavelink
-from discord.ext import commands
+from utils.subclasses.context import NexusContext
+from wavelink import Node, NodePool, Player, YouTubeTrack
+from wavelink.ext import spotify
+
+load_dotenv()
 
 
 class Music(Cog):
@@ -13,31 +21,55 @@ class Music(Cog):
         bot.loop.create_task(self.connect_nodes())
 
     async def connect_nodes(self):
-        """Connect to our Lavalink nodes."""
+        """
+        Connect to our Lavalink nodes.
+        """
         await self.bot.wait_until_ready()
 
-        await wavelink.NodePool.create_node(
-            bot=self.bot, host="127.0.0.1", port=2333, password="youshallnotpass"
+        await NodePool.create_node(
+            bot=self.bot,
+            host="127.0.0.1",
+            port=2333,
+            password="youshallnotpass",
+            spotify_client=spotify.SpotifyClient(
+                client_id=getenv("SPOTIFY_ID"),
+                client_secret=getenv("SPOTIFY_SECRET")
+            )
         )
 
-    @commands.Cog.listener()
-    async def on_wavelink_node_ready(self, node: wavelink.Node):
-        """Event fired when a node has finished connecting."""
-        print(f"Node: <{node.identifier}> is ready!")
-
-    @commands.command()
-    async def play(self, ctx: commands.Context, *, search: wavelink.YouTubeTrack):
-        """Play a song with the given search query.
-        If not connected, connect to our voice channel.
+    @Cog.listener(name="on_wavelink_node_ready")
+    async def _log_node_ready(self, node: Node):
         """
-        if not ctx.voice_client:
-            vc: wavelink.Player = await ctx.author.voice.channel.connect(
-                cls=wavelink.Player
-            )
-        else:
-            vc: wavelink.Player = ctx.voice_client
+        Event fired when a node has finished connecting.
+        """
+        self.bot.logger.info(f"Node: <{node.identifier}> is ready!")
 
-        await vc.play(search)
+    @guild_only()
+    @command(cls=Command, name="connect")
+    async def _connect(self, ctx: NexusContext, channel: Optional[VoiceChannel] = None):
+        """
+        Connect to a voice channel
+        
+        Defaults to your current channel if not specified
+        """
+        if (
+            ctx.voice_client
+            and len(ctx.voice_client.channel.members) > 1
+            and (
+                len(ctx.voice_client.channel.members) != 2
+                or ctx.author.id
+                not in [m.id for m in ctx.voice_client.channel.members]
+            )
+        ):
+            return await ctx.error("I am already connected to a voice channel!")
+
+        try:
+            channel = channel or ctx.author.voice.channel
+        except AttributeError:
+            return await ctx.error("Please specify a channel or join one!")
+
+        await channel.connect(cls=Player)
+                
 
 
 def setup(bot: Nexus):
