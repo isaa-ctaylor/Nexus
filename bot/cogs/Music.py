e8f6d72ec94f6b636f24bac35b0752c76c2880b2
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-11-03 19:03:52 +0000

    🔧 Rewritten to use pomice https://pomice.readthedocs.io/en/latest/pomice.html

--- bot/cogs/Music.py
+++ bot/cogs/Music.py
@@ -3,7 +3,7 @@ from asyncio import Queue
 from os import getenv
 from typing import Optional
 
-from discord.channel import VoiceChannel
+from discord.channel import TextChannel, VoiceChannel
 from discord.client import Client
 from discord.embeds import Embed
 from discord.ext.commands import command
@@ -23,26 +23,31 @@ from wavelink.utils import MISSING
 import async_timeout
 import math
 from typing import Union
+import pomice
 
 load_dotenv()
 
 
-class Player(Player):
+class Player(pomice.Player):
     def __init__(
         self,
         client: Client = MISSING,
         channel: VoiceChannel = MISSING,
         *,
-        node: Node = MISSING,
+        node: pomice.Node = MISSING,
     ):
-        self.control_channel = None
+        self.control_channel: TextChannel = None
         super().__init__(client=client, channel=channel, node=node)
         self.queue = Queue()
+        self.skippers = set()
 
 
 class Music(Cog):
     def __init__(self, bot: Nexus):
         self.bot = bot
+
+        if not hasattr(self.bot, "pomice"):
+            self.bot.pomice = pomice.NodePool()
 
         bot.loop.create_task(self.connect_nodes())
 
@@ -57,7 +62,7 @@ class Music(Cog):
         """
         await self.bot.wait_until_ready()
 
-        await NodePool.create_node(
+        await self.bot.pomice.create_node(
             bot=self.bot,
             host="127.0.0.1",
             port=2333,
@@ -67,29 +72,24 @@ class Music(Cog):
             ),
         )
 
-    @Cog.listener(name="on_wavelink_node_ready")
-    async def _log_node_ready(self, node: Node):
-        """
-        Event fired when a node has finished connecting.
-        """
-        self.bot.logger.info(f"Node: <{node.identifier}> is ready!")
-
-    @Cog.listener(name="on_wavelink_track_end")
-    async def _do_next_song(self, player: Player, track: Track, reason):
+    @Cog.listener(name="on_pomice_track_end")
+    async def _do_next_song(
+        self, event: pomice.PomiceEvent, player: Player, track: pomice.Track, reason
+    ):
         if reason not in ["FINISHED", "STOPPED", "SKIPPED"]:
             return
 
         if reason == "SKIPPED":
             await player.stop()
-        
+
         try:
             with async_timeout.timeout(300):
                 track = await player.queue.get()
-                await player.play(track)
-                return await player.control_channel.send(
-                    f"Now playing `{track.title}` | {track.requester.mention}",
-                    allowed_mentions=AllowedMentions.none(),
-                )
+            await player.play(track)
+            return await player.control_channel.send(
+                f"Now playing `{track.title}` | {track.requester.mention}",
+                allowed_mentions=AllowedMentions.none(),
+            )
         except asyncio.TimeoutError:
             await player.disconnect(force=True)
             return await player.control_channel.send("👋 Disconnected - queue finished")
@@ -188,34 +188,36 @@ class Music(Cog):
                 f"🔍 Searching for `{codeblocksafe(query)}`", mention_author=False
             )
 
-        track = None
-        try:
-            track = await SpotifyTrack.convert(ctx, query)
-        except SpotifyRequestError:
-            pass
+        player: Player = ctx.voice_client
+        tracks = await player.get_tracks(query=query, ctx=ctx)
 
-        if track is None:
-            try:
-                track = await YouTubeTrack.convert(ctx, query)
-            except BadArgument:
-                pass
+        if not tracks:
+            await ctx.send("❌ No results!")
 
-        if track is None:
-            try:
-                track = await SoundCloudTrack.convert(ctx, query)
-            except BadArgument:
-                return await ctx.error("Couldn't find any songs matching that query!")
+        if isinstance(tracks, pomice.Playlist):
+            await ctx.send(f"Adding {tracks.track_count} songs to the queue")
 
-        track.requester = ctx.author
-        track.skippers = {}
+            for track in tracks.tracks:
+                track.requester = ctx.author
+                await player.queue.put(track)
 
-        if not ctx.voice_client.is_playing() and not ctx.voice_client.is_paused():
-            await ctx.send(f"Playing `{codeblocksafe(track.title)}`")
-            await ctx.voice_client.play(track)
+            if not player.is_playing and not player.is_paused:
+                await player.play(player.queue.get_nowait(), ignore_if_playing=True)
+
+            else:
+                await player.control_channel.send(
+                    f"Enqueued {tracks.track_count} songs."
+                )
 
         else:
-            await ctx.send(f"Enqueued `{codeblocksafe(track.title)}`")
-            await ctx.voice_client.queue.put(track)
+            tracks[0].requester = ctx.author
+            if not player.is_playing and not player.is_paused:
+                await player.play(tracks[0])
+                await ctx.send(f"Playing `{codeblocksafe(tracks[0].title)}`")
+
+            else:
+                await player.queue.put(tracks[0])
+                await ctx.send(f"Enqueued `{codeblocksafe(tracks[0].title)}`")
 
     @guild_only()
     @command(cls=Command, name="stop")
@@ -232,8 +234,10 @@ class Music(Cog):
         if not ctx.author.voice:
             return await ctx.error("You are not in a voice channel!")
 
-        await ctx.voice_client.stop()
-        ctx.voice_client.queue._queue.clear()
+        player: Player = ctx.voice_client
+
+        await player.stop()
+        player.queue._queue.clear()
         await ctx.message.add_reaction("👍")
 
     @guild_only()
@@ -254,7 +258,9 @@ class Music(Cog):
         if not ctx.author.voice:
             return await ctx.error("You are not in a voice channel!")
 
-        await ctx.voice_client.disconnect(force=True)
+        player: Player = ctx.voice_client
+
+        await player.destroy()
         await ctx.message.add_reaction("👍")
 
     @guild_only()
@@ -263,21 +269,20 @@ class Music(Cog):
         """
         See the current queue
         """
-        if not ctx.voice_client:
+        player: Player = ctx.voice_client
+
+        if not player:
             return await ctx.error("I am not playing anything at the moment!")
 
-        if not len(ctx.voice_client.queue._queue):
-            return await ctx.error("Nothing in the queue!")
-
         pages = [
-            list(ctx.voice_client.queue._queue)[i : i + 10]
-            for i in range(0, len(ctx.voice_client.queue._queue), 10)
+            list(player.queue._queue)[i : i + 10]
+            for i in range(0, len(player.queue._queue), 10)
         ]
 
         embeds = [
             Embed(
                 description="\n".join(
-                    f"{list(ctx.voice_client.queue._queue).index(t) + 1}) {hyperlink(f'`{t.title}`', t.uri)}"
+                    f"{list(player.queue._queue).index(t) + 1}) {hyperlink(f'`{t.title}`', t.uri)}"
                     for t in l
                 ),
                 colour=self.bot.config.colours.neutral,
@@ -286,11 +291,11 @@ class Music(Cog):
         ]
 
         embeds[0].description = (
-            f"**Now playing**: {hyperlink(ctx.voice_client.track.title, ctx.voice_client.track.uri)}\n\n"
+            f"**Now playing**: {hyperlink(player.current.title, player.current.uri)}\n\n"
             + embeds[0].description.strip()
         )
 
-        if thumb := getattr(ctx.voice_client.track, "thumbnail", None):
+        if thumb := getattr(player.current, "thumbnail", None):
             embeds[0].set_thumbnail(url=thumb)
 
         await ctx.paginate(embeds)
@@ -301,15 +306,29 @@ class Music(Cog):
         """
         Remove a song from the queue
         """
+        player: Player = ctx.voice_client
+        
+        if not player:
+            return await ctx.error("I am not playing anything at the moment!")
+
+        if (
+            ctx.author.voice
+            and ctx.author.voice.channel.id != player.channel.id
+        ):
+            return await ctx.error("You are not in the same channel as me!")
+
+        if not ctx.author.voice:
+            return await ctx.error("You are not in a voice channel!")
+
         try:
-            track = list(ctx.voice_client.queue._queue)[index - 1]
+            track: Track = list(player.queue._queue)[index - 1]
         except IndexError:
             return await ctx.error("Please provide a valid song index!")
 
         if track.requester.id != ctx.author.id:
             return await ctx.error("You did not request this song!")
 
-        ctx.voice_client.queue._queue.remove(track)
+        player.queue._queue.remove(track)
         await ctx.reply(
             f"👍 Removed `{track.title}` from the queue", mention_author=False
         )
@@ -320,22 +339,24 @@ class Music(Cog):
         """
         Pause the current song
         """
-        if not ctx.voice_client:
+        player: Player = ctx.voice_client
+
+        if not player:
             return await ctx.error("I am not playing anything at the moment!")
 
         if (
             ctx.author.voice
-            and ctx.author.voice.channel.id != ctx.voice_client.channel.id
+            and ctx.author.voice.channel.id != player.channel.id
         ):
             return await ctx.error("You are not in the same channel as me!")
 
         if not ctx.author.voice:
             return await ctx.error("You are not in a voice channel!")
 
-        if ctx.voice_client.is_paused():
+        if player.is_paused:
             return await ctx.error("The player is already paused!")
 
-        await ctx.voice_client.pause()
+        await player.set_pause(True)
         await ctx.message.add_reaction("👍")
 
     @guild_only()
@@ -344,22 +365,24 @@ class Music(Cog):
         """
         Resume the paused song
         """
-        if not ctx.voice_client:
+        player: Player = ctx.voice_client
+        
+        if not player:
             return await ctx.error("I am not playing anything at the moment!")
 
         if (
             ctx.author.voice
-            and ctx.author.voice.channel.id != ctx.voice_client.channel.id
+            and ctx.author.voice.channel.id != player.channel.id
         ):
             return await ctx.error("You are not in the same channel as me!")
 
         if not ctx.author.voice:
             return await ctx.error("You are not in a voice channel!")
 
-        if not ctx.voice_client.is_paused():
+        if not player.is_paused():
             return await ctx.error("The player is not paused!")
 
-        await ctx.voice_client.resume()
+        await player.set_pause(False)
         await ctx.message.add_reaction("👍")
 
     @guild_only()
@@ -368,12 +391,13 @@ class Music(Cog):
         """
         See what song is currently playing
         """
-        if not ctx.voice_client:
+        player: Player = ctx.voice_client
+        if not player:
             return await ctx.error("I am not playing anything at the moment!")
 
         await ctx.embed(
             title="Currently playing",
-            description=f"{hyperlink(f'`{ctx.voice_client.track.title}`', ctx.voice_client.track.uri)} requested by {ctx.voice_client.track.requester.mention}",
+            description=f"{hyperlink(f'`{player.current.title}`', player.current.uri)} requested by {player.current.requester.mention}",
         )
 
     @guild_only()
@@ -382,64 +406,81 @@ class Music(Cog):
         """
         Skip the current song
         """
-        if not ctx.voice_client:
+        player: Player = ctx.voice_client
+        
+        if not player:
             return await ctx.error("I am not playing anything at the moment!")
 
         if (
             ctx.author.voice
-            and ctx.author.voice.channel.id != ctx.voice_client.channel.id
+            and ctx.author.voice.channel.id != player.channel.id
         ):
             return await ctx.error("You are not in the same channel as me!")
 
         if not ctx.author.voice:
             return await ctx.error("You are not in a voice channel!")
 
-        if ctx.author.id != ctx.voice_client.track.requester.id or not ctx.author.guild_permissions.manage_guild:
+        if (
+            ctx.author.id != player.current.requester.id
+            or not ctx.author.guild_permissions.manage_guild
+        ):
             required = self.required(ctx)
 
-            _ = ctx.voice_client.track.skippers.copy()
-            ctx.voice_client.track.skippers.add(ctx.author.id)
+            _ = player.skippers.copy()
+            player.skippers.add(ctx.author.id)
 
-            if ctx.voice_client.track.skippers == _:
-                return await ctx.send("You have already voted to skip!", mention_author=False)
+            if player.skippers == _:
+                return await ctx.send(
+                    "You have already voted to skip!", mention_author=False
+                )
 
-            if len(ctx.voice_client.track.skippers) < required:
-                return await ctx.reply(f"Voted to skip ({len(ctx.voice_client.track.skippers)}/{required})")
+            if len(player.skippers) < required:
+                return await ctx.reply(
+                    f"Voted to skip ({len(player.skippers)}/{required})"
+                )
+
+        player.skippers.clear()
 
         await ctx.send("⏭ Skipping")
 
         self.bot.dispatch(
-            "wavelink_track_end", ctx.voice_client, ctx.voice_client.track, "SKIPPED"
+            "pomice_track_end", ctx.voice_client, ctx.voice_client.track, "SKIPPED"
         )
-        
+
     @guild_only()
     @command(cls=Command, name="volume")
     async def _volume(self, ctx: NexusContext, volume: Union[int, str]):
         """
         Set the volume of the player
         """
-        if not ctx.voice_client:
+        player: Player = ctx.voice_client
+
+        if not player:
             return await ctx.error("I am not playing anything at the moment!")
 
         if (
             ctx.author.voice
-            and ctx.author.voice.channel.id != ctx.voice_client.channel.id
+            and ctx.author.voice.channel.id != player.channel.id
         ):
             return await ctx.error("You are not in the same channel as me!")
 
         if not ctx.author.voice:
             return await ctx.error("You are not in a voice channel!")
-        
+
         if isinstance(volume, str):
             if volume.lower() == "reset":
                 volume = 100
             else:
-                return await ctx.error("Please specify a number between 1 and 200, or \"reset\"")
-            
+                return await ctx.error(
+                    'Please specify a number between 1 and 200, or "reset"'
+                )
+
         if volume > 200 or volume < 1:
-            return await ctx.error("Please specify a number between 1 and 200, or \"reset\"")
-        
-        ctx.voice_client.set_volume(volume)
+            return await ctx.error(
+                'Please specify a number between 1 and 200, or "reset"'
+            )
+
+        player.set_volume(volume)
         await ctx.message.add_reaction("👍")
 
 
