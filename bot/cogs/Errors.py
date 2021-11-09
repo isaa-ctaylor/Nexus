7ca5d71bc885a3a8fc5a84ce372aeb1a63ee0121
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-11-09 07:43:33 +0000

    🔧 Hacky thing

--- bot/cogs/Errors.py
+++ bot/cogs/Errors.py
@@ -1,3 +1,4 @@
+import re
 from traceback import format_exception
 from discord.embeds import Embed
 from discord.errors import Forbidden
@@ -193,6 +194,11 @@ class Errors(Cog, hidden=True):
             )
 
         elif isinstance(error, commands.BadArgument):
+            match = re.match(r"Couldn't find the (?P<name>[a-zA-Z ]+)!", error.args[0])
+            
+            if match is not None:
+                return await ctx.error(f"{match.group('name')} is not a valid command!")
+                
             return await ctx.error(
                 error.args[0],
             )
