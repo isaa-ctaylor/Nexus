04489477a56dd94c3699748f88bd171b28903a9e
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-11-10 16:26:45 +0000

    🤔 Debugging

--- bot/utils/subclasses/command.py
+++ bot/utils/subclasses/command.py
@@ -78,16 +78,19 @@ class Group(DiscordGroup):
         )
 
         self.examples: Tuple[str] = tuple(examples)
-        print(permissions)
-        _p = set(permissions)
-        _p.add("send_messages")
-        permissions = sorted(_p)
         
-        _bp = set(bot_permissions)
-        _p.add("send_messages")
-        _p.add("embed_links")
-        bot_permissions = sorted(_bp)
+        try:
+            _p = set(permissions)
+            _p.add("send_messages")
+            permissions = sorted(_p)
             
+            _bp = set(bot_permissions)
+            _p.add("send_messages")
+            _p.add("embed_links")
+            bot_permissions = sorted(_bp)
+        except Exception as e:
+            print(e)
+        
         self.permissions: Tuple[str] = tuple(permissions)
         self.bot_permissions: Tuple[str] = tuple(bot_permissions)
 
