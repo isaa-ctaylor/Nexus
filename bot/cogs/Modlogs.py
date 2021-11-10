381532166803e5a292bd042e3c359ab540bfff87
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-11-10 17:37:17 +0000

    🤔 Debugging

--- bot/cogs/Modlogs.py
+++ bot/cogs/Modlogs.py
@@ -137,99 +137,99 @@ class Modlogs(Cog):
 
     @Cog.listener(name="on_message_delete")
     async def _log_message_delete(self, message: Message):
-        with suppress(Exception):
-            if not (
-                message.guild.id in self.cache
-                and self.cache[message.guild.id]["enabled"]
-            ):
-                return
+        # with suppress(Exception):
+        if not (
+            message.guild.id in self.cache
+            and self.cache[message.guild.id]["enabled"]
+        ):
+            return
 
-            channel = self.cache[message.guild.id]["channel"]
+        channel = self.cache[message.guild.id]["channel"]
 
-            if not channel:
-                return
+        if not channel:
+            return
 
-            channel = message.guild.get_channel(
-                channel
-            ) or await message.guild.fetch_channel(channel)
+        channel = message.guild.get_channel(
+            channel
+        ) or await message.guild.fetch_channel(channel)
 
-            embed = (
-                Embed(title="Modlog delete", colour=self.bot.config.colours.neutral)
-                .add_field(name="Channel", value=message.channel.mention)
-                .add_field(name="Author", value=message.author.mention)
-                .add_field(
-                    name="Content",
-                    value=message.content or "This message contained no content.",
-                    inline=False,
-                )
+        embed = (
+            Embed(title="Modlog delete", colour=self.bot.config.colours.neutral)
+            .add_field(name="Channel", value=message.channel.mention)
+            .add_field(name="Author", value=message.author.mention)
+            .add_field(
+                name="Content",
+                value=message.content or "This message contained no content.",
+                inline=False,
             )
+        )
 
-            await self._send(message.guild, embed=embed)
+        await self._send(message.guild, embed=embed)
 
     @Cog.listener(name="on_bulk_message_delete")
     async def _log_bulk_message_delete(self, messages: List[Message]):
-        with suppress(Exception):
-            if not (
-                messages[0].guild.id in self.cache
-                and self.cache[messages[0].guild.id]["enabled"]
-            ):
-                return
+        # with suppress(Exception):
+        if not (
+            messages[0].guild.id in self.cache
+            and self.cache[messages[0].guild.id]["enabled"]
+        ):
+            return
 
-            channel = self.cache[messages[0].guild.id]["channel"]
+        channel = self.cache[messages[0].guild.id]["channel"]
 
-            if not channel:
-                return
+        if not channel:
+            return
 
-            channel = messages[0].guild.get_channel(channel) or await messages[
-                0
-            ].guild.fetch_channel(channel)
+        channel = messages[0].guild.get_channel(channel) or await messages[
+            0
+        ].guild.fetch_channel(channel)
 
-            embed = (
-                Embed(
-                    title="Modlog bulk delete", colour=self.bot.config.colours.neutral
-                )
-                .add_field(name="Channel", value=messages[0].channel.mention)
-                .add_field(name="Quantity", value=len(messages))
-                .add_field(
-                    name="Authors",
-                    value=utils.naturallist(set([m.author for m in messages])),
-                    inline=False,
-                )
+        embed = (
+            Embed(
+                title="Modlog bulk delete", colour=self.bot.config.colours.neutral
             )
+            .add_field(name="Channel", value=messages[0].channel.mention)
+            .add_field(name="Quantity", value=len(messages))
+            .add_field(
+                name="Authors",
+                value=utils.naturallist(set([m.author for m in messages])),
+                inline=False,
+            )
+        )
 
-            await self._send(channel.guild, embed=embed)
+        await self._send(channel.guild, embed=embed)
 
     @Cog.listener(name="on_message_edit")
     async def _log_message_edit(self, before: Message, after: Message):
-        with suppress(Exception):
-            if before.content == after.content:
-                return
+        # with suppress(Exception):
+        if before.content == after.content:
+            return
 
-            if not (
-                after.guild.id in self.cache and self.cache[after.guild.id]["enabled"]
-            ):
-                return
+        if not (
+            after.guild.id in self.cache and self.cache[after.guild.id]["enabled"]
+        ):
+            return
 
-            channel = self.cache[after.guild.id]["channel"]
+        channel = self.cache[after.guild.id]["channel"]
 
-            if not channel:
-                return
+        if not channel:
+            return
 
-            channel = after.guild.get_channel(
-                channel
-            ) or await after.guild.fetch_channel(channel)
+        channel = after.guild.get_channel(
+            channel
+        ) or await after.guild.fetch_channel(channel)
 
-            embed = (
-                Embed(
-                    title="Modlog message edit", colour=self.bot.config.colours.neutral
-                )
-                .add_field(name="Channel", value=after.channel.mention)
-                .add_field(name="Author", value=after.author.mention)
-                .add_field(name="Content before", value=before.content, inline=False)
-                .add_field(name="Content after", value=after.content, inline=True)
+        embed = (
+            Embed(
+                title="Modlog message edit", colour=self.bot.config.colours.neutral
             )
+            .add_field(name="Channel", value=after.channel.mention)
+            .add_field(name="Author", value=after.author.mention)
+            .add_field(name="Content before", value=before.content, inline=False)
+            .add_field(name="Content after", value=after.content, inline=True)
+        )
 
-            await self._send(channel.guild, embed=embed)
+        await self._send(channel.guild, embed=embed)
 
 
 def setup(bot: Nexus):
