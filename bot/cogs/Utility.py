017ee78ade61b46762f9b039c4de429f2f93d380
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2021-12-24 15:38:16 +0000

    🔧 Fixed maybe

--- bot/cogs/Utility.py
+++ bot/cogs/Utility.py
@@ -630,7 +630,7 @@ class Utility(Cog):
 
     @tasks.loop(minutes=1)
     async def _send_reminders(self):
-        now = datetime.datetime.utcnow()
+        now = datetime.datetime.now()
         data = await self.bot.db.fetch(
             "SELECT * FROM reminders WHERE (timeend - $1) <= 60",
             int(now.timestamp()),
