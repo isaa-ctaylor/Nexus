7ab533a8e273b25d291e369f06a5ad02bc5bbc8a
isaa-ctaylor <64303279+isaa-ctaylor@users.noreply.github.com>
2022-04-29 07:48:35 +0100

    🔧 Testing newmusic connection to lavalink

---
+++ bot/cogs/NewMusic.py
@@ -0,0 +1,19 @@
+from utils.subclasses.cog import Cog
+from utils.subclasses.bot import Nexus
+from utils.subclasses.context import NexusContext
+import wavelink
+
+
+class NewMusic(Cog):
+    def __init__(self, bot: Nexus):
+        self.bot = bot
+        
+        self.bot.loop.create_task(self._connect_node())
+    
+    async def _connect_node(self):
+        await self.bot.wait_until_ready()
+
+        if not getattr(self.bot, "wavelink"):
+            self.bot.wavelink = wavelink.NodePool.create_node(bot=self.bot, host="127.0.0.1", password="youshallnotpass")
+        
+    
